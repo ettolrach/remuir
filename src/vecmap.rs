082f68dee4569aa6aaf080196cd4334b0d8639ac
ettolrach <ettolrach@disroot.org>
2024-02-07 03:35:09 +0000

    Add licensing information

--- src/vecmap.rs
+++ src/vecmap.rs
@@ -1,3 +1,11 @@
+/* This file is part of remuir.
+
+remuir is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+remuir is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with remuir. If not, see <https://www.gnu.org/licenses/>. */
+
 #[derive(Default, Debug, PartialEq)]
 pub struct VecMap<K, V> {
     pub vec: Vec<(K, V)>
