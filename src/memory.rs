ca4bc81ecf42abae85c561a3ae13c45c81363406
ettolrach <ettolrach@disroot.org>
2024-06-17 20:38:45 +0100

    Move Memory/Register tests to in-file tests

--- src/memory.rs
+++ src/memory.rs
@@ -224,3 +224,35 @@ impl FromIterator<Register> for Memory {
         Memory { nat_registers: Vec::from_iter(iter), neg_registers: Vec::new() }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn inc_from_max_test() {
+        let mut reg = Register::new(&[u128::MAX][..]);
+        reg.inc();
+        assert_eq!(reg, Register::new(&[0, 1]));
+    }
+
+    #[test]
+    fn inc_units_max_only_test() {
+        let mut reg = Register::new(&[u128::MAX, u128::MAX, 4, ]);
+        reg.inc();
+        assert_eq!(reg, Register::new(&[0, 0, 5]));
+    }
+
+    #[test]
+    fn dec_from_0_units() {
+        let mut reg = Register::new(&[0, 1]);
+        reg.dec();
+        assert_eq!(reg, Register::new(&[u128::MAX]));
+    }
+
+    #[test]
+    fn is_zero_test() {
+        let reg = Register::new(&[]);
+        let mut mem = Memory::new_from_slice(&[reg]);
+        assert!(mem.is_zero(RegisterNumber::Natural(0)))
+    }
+}
